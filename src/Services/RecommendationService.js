8fe80db19937050090f2ac91ac33f7226460ad2a
Ryan Pairitz <rpairitz@db.cse.nd.edu>
2021-12-11 22:55:55 -0500

    recommendation service with axios

---
+++ src/Services/RecommendationService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import * as Env from "../environments";
+
+// API_PATH var to hold backend endpoint 
+const API_PATH = Env.API_PATH;
+
+export const getRecommended = (songId) => {
+   // use axios to interface with backend to issue queries and retrieve async data from db
+   // format params to JSON object to be sent using axios and received by PHP in proper format for decoding
+   return axios.get(API_PATH, {params: {id: songId}}).then((response) => {
+       console.log(response);
+       return response.data;
+   })
+   .catch((error) => {
+       alert(`Error: ${error.message}`);
+   });
+};
